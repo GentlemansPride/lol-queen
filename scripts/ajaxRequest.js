6f988e9d9ff266301e7078f205a8287afb389065
5ia15adbuechi <5ia15adbuechi@gibwintra.ch>
2017-12-04 16:59:53 +0100

    added simple get functions

--- scripts/ajaxRequest.js
+++ scripts/ajaxRequest.js
@@ -1,13 +1,23 @@
 const API_KEY = "RGAPI-67532232-2166-4447-852c-330327f03937";
 
-function requestData() {
+function requestSummonerData(requestUrl) {
     $.ajax({
-        type: "POST",
-        url: "",
+        type: "GET",
+        url: requestUrl,
         success: function () {
 
         }, error: function () {
 
         }
     });
+}
+
+$("#searchSummoner").on("click", function () {
+    let summoner = $("#summonerSearchInput").val();
+    let requestUrl = concatApiString(summoner);
+    requestSummonerData(requestUrl);
+});
+
+function concatApiString(name) {
+    return String.concat("https://euw1.api.riotgames.com/lol/summoner/v3/summoners/by-name/", name, "?api_key=", API_KEY);
 }
