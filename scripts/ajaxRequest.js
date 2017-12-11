aee2b59d94e9a017d9a073d40cb894e0b778b7da
Ahmed Bajra <ahmed.bajra@gmail.com>
2017-12-11 15:17:06 +0100

    Added some ajax and handlebars magic

--- scripts/ajaxRequest.js
+++ scripts/ajaxRequest.js
@@ -1,23 +1,29 @@
 const API_KEY = "RGAPI-67532232-2166-4447-852c-330327f03937";
+let summonerData;
+let recentMatches;
 
-function requestSummonerData(requestUrl) {
-    $.ajax({
-        type: "GET",
-        url: requestUrl,
-        success: function () {
-
-        }, error: function () {
-
+function requestSummonerData(summonerName) {
+    $.getJSON("https://euw1.api.riotgames.com/lol/summoner/v3/summoners/by-name/" + summonerName + "?api_key=" + API_KEY,
+        function (data) {
+            return JSON.parse(data);
         }
-    });
+    );
 }
 
-$("#searchSummoner").on("click", function () {
-    let summoner = $("#summonerSearchInput").val();
-    let requestUrl = concatApiString(summoner);
-    requestSummonerData(requestUrl);
+function getRecentMatches(summonerID) {
+    $.getJSON("https://euw1.api.riotgames.com/lol/match/v3/matchlists/by-account/" + summonerID + "/recent?api_key=RGAPI-06207fa7-aa25-4623-bf84-23ec9dde9aee",
+        function (data) {
+            return JSON.parse(data);
+        }
+    );
+}
+
+$("#searchSummoner").click(function () {
+    let summonerName = $("#summonerSearchInput").val();
+    summonerData = requestSummonerData(summonerName);
+    $("#main").load("./pages/recentmatches.html", function () {
+        $(document).attr("title", "LoLQueen - Recent Matches");
+        recentMatches = getRecentMatches(summonerData.accountId);
+    });
 });
 
-function concatApiString(name) {
-    return String.concat("https://euw1.api.riotgames.com/lol/summoner/v3/summoners/by-name/", name, "?api_key=", API_KEY);
-}
