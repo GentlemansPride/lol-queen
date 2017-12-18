565d0b512b21e71595a4b77a3aec301a30e3295b
Ahmed Bajra <ahmed.bajra@gmail.com>
2017-12-18 16:57:08 +0100

    Fixed template + refactoring

--- scripts/ajaxRequest.js
+++ scripts/ajaxRequest.js
@@ -1,26 +1,32 @@
 const API_KEY = "RGAPI-67532232-2166-4447-852c-330327f03937";
 const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
-let summonerData;
-let recentMatches;
 
-function requestSummonerData(summonerName, callbackFn) {
+function requestSummonerData(summonerName, callback) {
     $.getJSON(CORS_PROXY + "https://euw1.api.riotgames.com/lol/summoner/v3/summoners/by-name/" + summonerName + "?api_key=" + API_KEY,
         function (data) {
-            callbackFn(data);
+            callback(data);
+        }
+    );
+}
+
+function requestRecentMatches(accountId, callback) {
+    $.getJSON(CORS_PROXY + "https://euw1.api.riotgames.com/lol/match/v3/matchlists/by-account/" + accountId + "/recent?api_key=" + API_KEY,
+        function (data) {
+            console.log(data);
+            callback(data)
         }
     );
 }
 
 function searchSummoner() {
     let summonerName = $("#summonerSearchInput").val();
-    summonerData = requestSummonerData(summonerName, function (summonerData) {
-        $.getJSON(CORS_PROXY + "https://euw1.api.riotgames.com/lol/match/v3/matchlists/by-account/" + summonerData.accountId + "/recent?api_key=RGAPI-06207fa7-aa25-4623-bf84-23ec9dde9aee",
-            function (data) {
-                console.log(data);
-                $("#main").load("./pages/recentmatches.html", function () {
-                    $(document).attr("title", "LoLQueen - Recent Matches");
-                });
-            }
-        );
+    requestSummonerData(summonerName, function (summonerData) {
+        requestRecentMatches(summonerData.accountId, function (recentMatches) {
+            $("#main").load("./pages/recentmatches.html", function () {
+                $(document).attr("title", "LoLQueen - Recent Matches");
+            });
+            let template = Handlebars.compile($("#recent-matches").html());
+            template(recentMatches);
+        })
     });
 }
