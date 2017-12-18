b22e6d59029c48fbc3b7f4ff53d178ce0eac59ea
TheOddPanda <adrianbuechi@gmail.com>
2017-12-18 16:36:26 +0100

    fixed async callback request

--- scripts/ajaxRequest.js
+++ scripts/ajaxRequest.js
@@ -3,29 +3,24 @@ const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
 let summonerData;
 let recentMatches;
 
-async function requestSummonerData(summonerName) {
+function requestSummonerData(summonerName, callbackFn) {
     $.getJSON(CORS_PROXY + "https://euw1.api.riotgames.com/lol/summoner/v3/summoners/by-name/" + summonerName + "?api_key=" + API_KEY,
         function (data) {
-        console.log(data);
-            return data;
+            callbackFn(data);
         }
     );
 }
 
-async function getRecentMatches(summonerID) {
-    $.getJSON(CORS_PROXY + "https://euw1.api.riotgames.com/lol/match/v3/matchlists/by-account/" + summonerID + "/recent?api_key=RGAPI-06207fa7-aa25-4623-bf84-23ec9dde9aee",
-        function (data) {
-        console.log(data);
-            return data;
-        }
-    );
-}
-
-async function searchSummoner() {
+function searchSummoner() {
     let summonerName = $("#summonerSearchInput").val();
-    summonerData = await requestSummonerData(summonerName);
-    $("#main").load("./pages/recentmatches.html", function () {
-        $(document).attr("title", "LoLQueen - Recent Matches");
-        recentMatches = getRecentMatches(summonerData.accountId);
+    summonerData = requestSummonerData(summonerName, function (summonerData) {
+        $.getJSON(CORS_PROXY + "https://euw1.api.riotgames.com/lol/match/v3/matchlists/by-account/" + summonerData.accountId + "/recent?api_key=RGAPI-06207fa7-aa25-4623-bf84-23ec9dde9aee",
+            function (data) {
+                console.log(data);
+                $("#main").load("./pages/recentmatches.html", function () {
+                    $(document).attr("title", "LoLQueen - Recent Matches");
+                });
+            }
+        );
     });
 }
