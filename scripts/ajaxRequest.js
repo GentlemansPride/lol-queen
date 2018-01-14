8939fa6a735c87d96ed11e84e324a84b8cd2f27f
Ahmed Bajra <ahmed.bajra@gmail.com>
2018-01-14 16:32:34 +0100

    Added champion-mapper for (id -> champion name) conversion

--- scripts/ajaxRequest.js
+++ scripts/ajaxRequest.js
@@ -1,5 +1,12 @@
 const API_KEY = "RGAPI-67532232-2166-4447-852c-330327f03937";
 const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
+let CHAMPS;
+
+$(function () {
+    $.getJSON("./data/champions.json", function (data) {
+        CHAMPS = data;
+    })
+});
 
 function requestSummonerData(summonerName, callback) {
     $.getJSON(CORS_PROXY + "https://euw1.api.riotgames.com/lol/summoner/v3/summoners/by-name/" + summonerName + "?api_key=" + API_KEY,
@@ -22,6 +29,13 @@ function searchSummoner() {
     $("#main").empty().load("./pages/loading.html");
     requestSummonerData(summonerName, function (summonerData) {
         requestRecentMatches(summonerData.accountId, function (recentMatches) {
+            let matches = [];
+            recentMatches.forEach(match => matches.push({
+                lane: match.lane,
+                champion: CHAMPS[match.champion],
+                season: match.season,
+                role: match.role
+            }));
             $("#main")
                 .empty()
                 .load("./pages/recentmatches.html", function () {
